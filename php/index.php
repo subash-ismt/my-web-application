de7a0be50bca7cd78f768e0abddb498bc7683554
subash-ismt <subash.tharu@ismt.edu.np>
2025-09-04 06:28:12 +0545

    Update and rename index.php to php/index.php

---
+++ php/index.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+echo "<h1>This is a heading</h1>";
+
+
+echo "<p>This is a paragraph.</p>";
+echo "<a href='greet.php?msg=hellow world'>This is a link</a>";
+
+
+?>
+</body>
+</html>
